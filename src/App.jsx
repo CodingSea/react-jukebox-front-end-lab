cc8a4e9ff8d5308cd6078b2f1729d5172724e48b
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-08-15 22:16:27 +0300

    fixed code to match "React Router style"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import { useState, useEffect } from "react";
 import Home from "./components/Home/Home";
 import "./App.css";
+import NewTrackBtn from "./components/NewTrackBtn/NewTrackBtn";
+import TrackForm from "./components/TrackForm/TrackForm";
+import { getAllTracks, getTrack } from "../lib/api";
+
+
 
 const App = () =>
 {
+  const [tracks, setTracks] = useState([]);
+  const [isFormShown, setIsFormShown] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [trackId, setTrackId] = useState({});
+
+  const [playingTrack, setPlayingTrack] = useState(
+    {
+      title: "",
+      artist: ""
+    }
+  )
+
+  function handleShowForm()
+  {
+    setIsFormShown(!isFormShown);
+  }
+
+  async function listAllTracks()
+  {
+    const response = await getAllTracks();
+    setTracks(response);
+  }
+
   return (
     <>
-      <Home />
+      <NewTrackBtn handleShowForm={ handleShowForm } setEditMode={ setEditMode } />
+      {
+        isFormShown
+          ?
+          <TrackForm listAllTracks={ listAllTracks } setIsFormShown={ setIsFormShown } editMode={ editMode } trackId={ trackId } />
+          :
+          null
+      }
+      <Home handleShowForm={ handleShowForm } listAllTracks={ listAllTracks } setEditMode={ setEditMode } setTrackId={ setTrackId } playingTrack={ playingTrack } setPlayingTrack={ setPlayingTrack } tracks={tracks} />
     </>
   )
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,35 +1,12 @@
 import React, { useState } from 'react';
 import TrackList from '../TrackList/TrackList';
-import NewTrackBtn from '../NewTrackBtn/NewTrackBtn';
 import NowPlaying from '../PlayingTrack/NowPlaying';
 import TrackForm from '../TrackForm/TrackForm';
 import { getAllTracks, getTrack } from '../../../lib/api';
 
-function Home()
+function Home({setEditMode, setTrackId, handleShowForm, listAllTracks, playingTrack, setPlayingTrack, tracks})
 {
-    const [tracks, setTracks] = useState([]);
-    const [isFormShown, setIsFormShown] = useState(false);
-    const [editMode, setEditMode] = useState(false);
-    const [trackId, setTrackId] = useState({});
-
     const [isTrackPlaying, setIsTrackPlaying] = useState(false);
-    const [playingTrack, setPlayingTrack] = useState(
-        {
-            title: "",
-            artist: ""
-        }
-    )
-
-    function handleShowForm()
-    {
-        setIsFormShown(!isFormShown);
-    }
-
-    async function listAllTracks()
-    {
-        const response = await getAllTracks();
-        setTracks(response);
-    }
 
     async function handlePlayigTrack(id)
     {
@@ -64,14 +41,6 @@ function Home()
 
     return (
         <>
-            <NewTrackBtn handleShowForm={ handleShowForm } setEditMode={ setEditMode } />
-            {
-                isFormShown
-                    ?
-                    <TrackForm listAllTracks={ listAllTracks } setIsFormShown={ setIsFormShown } editMode={ editMode } trackId={ trackId } />
-                    :
-                    null
-            }
             <TrackList tracks={ tracks } listAllTracks={ listAllTracks } handleShowForm={ handleShowForm } setEditMode={ setEditMode } setTrackId={ setTrackId } handlePlayigTrack={ handlePlayigTrack } />
             {
                 isTrackPlaying
